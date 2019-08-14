fb5c18d4dd5c5e5a8c10e17f31d14fa4da77fa8d
Marivaldo Rosa <marivaldok@hotmail.com>
2019-08-14 14:26:38 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -1,10 +1,37 @@
 <?php
 require_once("config.php");
 
-$root = new Usuario();
+// Carrega um usuario
 
-$root->loadById(2);
+//$root = new Usuario();
 
-echo $root;
+//$root->loadById(2);
+
+//echo $root;
+
+//=======================================
+
+//Carrega uma lista de usuarios
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//========================================
+
+//Carrega uma lista de usuarios buscando pelo login
+
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+//=======================================
+
+// Carrega um usuario usando login e senha.
+$usuario = new Usuario();
+
+$usuario->login("Marivaldo", "123456");
+
+echo $usuario;
 
 ?>
