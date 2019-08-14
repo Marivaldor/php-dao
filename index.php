3317ea6ca9e60355533dbdd18c736b8d10bf553f
Marivaldo Rosa <marivaldok@hotmail.com>
2019-08-14 18:34:14 -0300

    Comando Delete com o Comando Update

--- index.php
+++ index.php
@@ -28,9 +28,41 @@ require_once("config.php");
 //=======================================
 
 // Carrega um usuario usando login e senha.
+//$usuario = new Usuario();
+
+//$usuario->login("Marivaldo", "123456");
+
+//echo $usuario;
+
+//======================================
+
+//Insere um usuario no banco de dados.
+
+//$aluno = new Usuario();
+
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("@lun0");
+
+//$aluno->insert();
+
+//echo $aluno;
+
+//=========================================
+// Alterar um Usuario.
+/*$usuario = new Usuario();
+
+$usuario->loadById(5);
+
+$usuario->update("Professor", "brasil123");
+
+echo $usuario;
+*/
+
 $usuario = new Usuario();
 
-$usuario->login("Marivaldo", "123456");
+$usuario->loadById(9);
+
+$usuario->delete();
 
 echo $usuario;
 
